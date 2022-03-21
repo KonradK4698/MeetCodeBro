931d0030dc389804ef6e82f6a02bac8de70c9993
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-03-21 12:33:48 +0100

    neo4j connection test

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 const main = require('./routes/main');
+const neo4j = require('neo4j-driver')
+
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'Kwakus1998'))
+
+const session = driver.session()
+
+session
+  .run('MATCH (konrad {name:"Konrad"}) RETURN konrad')
+  .then(result => {
+    result.records.forEach(record => {
+      console.log(record.get('konrad').properties.name)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  .then(() => session.close())
 
 //main page router
 app.use('/', main);
