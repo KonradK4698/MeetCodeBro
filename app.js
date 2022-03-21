6f25e860937392f639cc6beb60edea40347da93b
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-03-21 14:58:01 +0100

    test register user in db

--- app.js
+++ app.js
@@ -4,30 +4,22 @@ const port = 3000;
 const main = require('./routes/main');
 const register = require('./routes/register')
 const neo4j = require('neo4j-driver')
+const bodyParser = require('body-parser')
 
 const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'Kwakus1998'))
 
 const session = driver.session()
 
-session
-  .run('MATCH (konrad {name:"Konrad"}) RETURN konrad')
-  .then(result => {
-    result.records.forEach(record => {
-      console.log(record.get('konrad'))
-    })
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  .then(() => session.close())
 
 //main page router
 app.use('/', main);
 
+app.use(express.json())
 //register page router
-app.use('/', register);
+app.use('/register', register);
 
 
 app.listen(port, () => {
     console.log("Aplikacja uruchomiona");
 });
+
--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const router = express.Router();
 
+    const express = require('express');
+    const router = express.Router();
+    const neo4j = require('neo4j-driver')
+const bodyParser = require('body-parser')
 
-router.post('/register', (req,res)=>{
-    console.log(req.body)
-    res.send("Konrad")
-});
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'Kwakus1998'))
+
+const session = driver.session()
+    
+    const addUser = (userName, userSurname) => {
+        session
+        .run(`CREATE (p:Person {name:"${userName}", surname:"${userSurname}"})`)
+        .catch(error => {
+            console.log(error)
+        })
+        .then(() => session.close())
+    }
+    
+    
+    router.post('/', (req,res)=>{
+        const userName = req.body.name;
+        const userSurname = req.body.surname;
+        addUser(userName, userSurname)
+        res.end();
+    });
+
 
 module.exports = router;
+
