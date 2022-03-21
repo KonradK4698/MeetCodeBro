c9083b8842648641fb9bd34c1651325cb13fed38
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-03-21 11:46:04 +0100

    add comment

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000; 
 const main = require('./routes/main');
 
+//main page router
 app.use('/', main);
+
 
 app.listen(port, () => {
     console.log("Aplikacja uruchomiona");
