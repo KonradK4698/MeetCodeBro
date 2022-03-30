a8c77e0d9ca304045e6a7026269f5fb9b0433442
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-03-30 15:28:17 +0200

    add comment in file

--- routes/register.js
+++ routes/register.js
@@ -3,17 +3,18 @@
     const router = express.Router();
     const session = require('../db')
 
-
+    //add new user to neo4j database
     const addUser = (userName, userSurname) => {
+        
         session
-        .run(`CREATE (p:Person {name:"${userName}", surname:"${userSurname}"})`)
+        .run(`CREATE (u:User {name:"${userName}", surname:"${userSurname}"})`)
         .catch(error => {
             console.log(error)
         })
         .then(() => session.close())
     }
     
-    
+    //add new user to db -  route
     router.post('/', (req,res)=>{
         const userName = req.body.name;
         const userSurname = req.body.surname;
