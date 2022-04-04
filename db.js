563baf8024bc0102b9487e1b60d311dc2c0f3ba9
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-04 11:45:05 +0200

    use .env file to storage DB login info

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const neo4j = require('neo4j-driver')
-const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'Kwakus1998'));
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic(process.env.DB_LOGIN, process.env.DB_PASSWORD));
 
 const session = driver.session();
 
-console.log("Nawiązano połączenie z bazą danych")
+console.log("Nawiązano połączenie z bazą danych");
 
  module.exports = session;
