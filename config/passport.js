3848062725d0e5e9435d0907ae5fb9dccb9a5832
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-10 23:51:58 +0200

    changes in session

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const fs = require('fs');
-const session = require('../db');
+const driver = require('../db');
 const path = require('path');
 
 
@@ -19,6 +19,7 @@ const options = {
 
 module.exports = (passport) => {
     passport.use(new JwtStrategy(options, function(jwt_payload, done){
+        const session = driver.session();
         session.run(`MATCH (u:User {email: "${jwt_payload.sub}" }) RETURN u AS user`).subscribe({
             onNext: record => { const user = record.get('user').properties; done(null, user);},
             onCompleted: () => { session.close()},
