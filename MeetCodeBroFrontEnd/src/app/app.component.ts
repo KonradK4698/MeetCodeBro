28086104350e218f305b876f5b57a0eec2d524e2
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-10 23:55:09 +0200

    change in main component

--- MeetCodeBroFrontEnd/src/app/app.component.ts
+++ MeetCodeBroFrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MeetCodeBroFrontEnd';
+  userLogged: boolean = false;
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.userLogged = this.authService.isLoggedIn();
+  }
+
+  logOut(): void{
+    this.authService.logout();
+  }
+
+  
+  
 }
