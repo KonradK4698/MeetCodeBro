fa8ce99faa6a0c4b8df0f5b4992cc05d529db401
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-10 23:54:22 +0200

    user login authentication

--- MeetCodeBroFrontEnd/src/app/app.module.ts
+++ MeetCodeBroFrontEnd/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthService } from './auth.service';
+import { AuthInterceptor } from './auth-interceptor';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +25,14 @@ import { LoginComponent } from './login/login.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor, 
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MeetCodeBroFrontEnd/src/app/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+  setLocalStorage(responseObj: any): void{
+    const expiresAt = moment().add(responseObj.token.momentNumber, responseObj.token.momentKey);
+    localStorage.setItem('id_token', responseObj.token.token);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout(): void {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    this.router.navigate(['/login']);
+    window.location.reload();
+  }
+
+  
+
+  public isLoggedIn() {
+      return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut(){
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration: any = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+  
+}
