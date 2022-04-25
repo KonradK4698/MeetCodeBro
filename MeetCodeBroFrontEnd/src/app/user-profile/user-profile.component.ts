8b67d0a3679e867909940353919b06084ab095ed
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-25 16:05:28 +0200

    adding material design and necessary functions

--- MeetCodeBroFrontEnd/src/app/user-profile/user-profile.component.ts
+++ MeetCodeBroFrontEnd/src/app/user-profile/user-profile.component.ts
@@ -7,6 +7,19 @@ import jwt_decode from 'jwt-decode';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { Technology } from '../technology';
 import { AuthService } from '../auth.service';
+import { FriendsService } from '../friends.service';
+import { decodeJWTToken } from '../decodeToken';
+import { Friend } from '../friend';
+
+interface Invite{
+  invitationID: number,
+  senderID: number
+}
+
+interface UserWithInvite{
+  senderInformation: User,
+  invitationInformation: Invite
+}
 
 @Component({
   selector: 'app-user-profile',
@@ -14,12 +27,20 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./user-profile.component.scss']
 })
 
+
+
 export class UserProfileComponent implements OnInit {
 
   message: string = '';
   value:boolean = true
   selectedItems = [];
   private choosenTechnologies: Technology[] = [];
+  public invitationSenderInfoArr: User[] = []; 
+  private invitationsArr: Invite[] = [];
+  public invitationSenders: UserWithInvite[] = [];
+  public userFriends: Friend[] = [];
+
+  userInformation: User = {};
 
 
   userProfileForm = new FormGroup({
@@ -30,9 +51,14 @@ export class UserProfileComponent implements OnInit {
     userLinkedin: new FormControl('')
   })
 
+  technologies = new FormControl();
+  
+  
+
   public result: Technology[] = [];
   
 
+  selected: number[] = [5, 6];
   
   dropdownSettings:IDropdownSettings = {
     singleSelection: false,
@@ -45,7 +71,7 @@ export class UserProfileComponent implements OnInit {
   };
   
 
-  constructor(private userProfileService: UserProfileService,private authService: AuthService){}
+  constructor(private userProfileService: UserProfileService,private authService: AuthService, private friendsService: FriendsService){}
   
   ngOnInit(): void {
     this.getUserData();
@@ -55,6 +81,9 @@ export class UserProfileComponent implements OnInit {
     
     this.getTechnologies();
 
+    this.getInvitation();
+
+    this.getAllFriends();
   }
 
   ngAfterViewInit(): void{
@@ -63,7 +92,7 @@ export class UserProfileComponent implements OnInit {
 
   getTechnologies(): void{
       this.userProfileService.getTechnologies().subscribe({
-      next: (data) => {this.result = data},
+      next: (data) => {this.result = data; console.log(data)},
       complete: () => {console.log("pobrano technologie");},
       error: (err) => {console.log(err);}
     })
@@ -93,25 +122,11 @@ export class UserProfileComponent implements OnInit {
 
   getUserData(): void{
 
-    interface lowHighObj {
-      low: number, 
-      high: number
-    }
-
-    interface JwtUserData {
-      sub: lowHighObj, 
-      exp: number,
-      iat: number
-    }
-
-    const token:string = <string>localStorage.getItem("id_token");
-    const tokenWithoutBearer:string = token.replace('Bearer ', '');
-    const decodedJWT:JwtUserData = jwt_decode(tokenWithoutBearer);
-    const userID = decodedJWT.sub.low;
+    const userID = decodeJWTToken().sub.low;
 
 
     this.userProfileService.getUserData(userID).subscribe({
-      next: (data) => {  this.setValueInForm(data) },
+      next: (data) => {  this.userInformation = data; this.setValueInForm(data) },
       complete: () => {console.log("Zakończono pobieranie dnaych"); this.message = "posiadasz dostęp"},
       error: (err) => {console.log(err); this.message = "brak dostępu";}
     })
@@ -119,21 +134,7 @@ export class UserProfileComponent implements OnInit {
   }
 
   addUserData(): void{
-    interface lowHighObj {
-      low: number, 
-      high: number
-    }
-
-    interface JwtUserData {
-      sub: lowHighObj, 
-      exp: number,
-      iat: number
-    }
-
-    const token:string = <string>localStorage.getItem("id_token");
-    const tokenWithoutBearer:string = token.replace('Bearer ', '');
-    const decodedJWT:JwtUserData = jwt_decode(tokenWithoutBearer);
-    const userID = decodedJWT.sub.low;
+    const userID = decodeJWTToken().sub.low;
     
     const userInfo: User = {
       name: this.userProfileForm.value.userName,
@@ -150,7 +151,113 @@ export class UserProfileComponent implements OnInit {
     } 
     )
 
-    this.addUserTechnologies(userID, this.choosenTechnologies);
+    //this.addUserTechnologies(userID, this.choosenTechnologies);
   }
 
+  getSenderInformation(invitationsArr: Invite[]): void{
+    
+    const idArray: number[] = [];
+    let finish:boolean = false;
+
+    invitationsArr.map(invitation => idArray.push(invitation.senderID));
+
+    this.userProfileService.getUserArray(idArray).subscribe({
+      next: (data) => {data.map(user => {this.invitationSenderInfoArr.push(user)})},
+      complete: () => {
+        finish = true; 
+        if(finish){
+          invitationsArr.forEach((invitation, index) => {
+            const newInvitationSender: UserWithInvite = {
+              senderInformation: this.invitationSenderInfoArr[index],
+              invitationInformation: invitation
+            }
+            this.invitationSenders.push(newInvitationSender);
+          })
+        }
+      },
+      error: (err) => {console.log(err)}
+    })
+    
+    
+  }
+
+  getInvitation(): void{
+    const userID = decodeJWTToken().sub.low;
+
+    this.friendsService.getFriendInvitation(userID).subscribe({
+      next: (invitations) => {
+        invitations.forEach(invitation => {
+          this.invitationsArr.push(
+            {
+              invitationID: invitation.identity.low,
+              senderID: invitation.start.low
+            }
+          )
+        })
+      },
+      complete: () => {
+        console.log("Pobrano zaproszenia");
+        
+        this.getSenderInformation(this.invitationsArr);
+      },
+      error: (err) => { console.log(err) }
+    })
+
+  }
+
+  deleteInvitation(invitationID: number) :void{
+    const userID = decodeJWTToken().sub.low;
+    
+    this.userProfileService.deleteInvitation(userID, invitationID).subscribe({
+      next: (data) => {console.log(data)},
+      complete: () => {console.log("usunięto relację");},
+      error: (err) => {console.log(err)}
+    })
+
+
+  }
+
+  getAllFriends(): void{
+    const userID = decodeJWTToken().sub.low;
+    const userFriendsID: number[] = [];
+
+    this.userProfileService.getAllFriends(userID).subscribe({
+        next: (data) => {data.map(relation => userFriendsID.push(relation.friendID))},
+        complete: () => {
+          this.userProfileService.getUserArray(userFriendsID).subscribe({
+            next: (users) => {users.forEach((user, id) => {
+              const newFriend :Friend = {
+                userInformation: user,
+                friendID: userFriendsID[id]
+              }
+
+              this.userFriends.push(newFriend)}
+              )},
+            complete: () => {console.log("Pobrano przyjaciół")},
+            error: (err) => {console.log(err)}
+          })
+        },
+        error: (err) => {console.log(err)}
+    })
+  }
+
+
+  deleteFriend(friendID: number): void{
+    const userID = decodeJWTToken().sub.low;
+
+    this.userProfileService.deleteFriend(userID, friendID).subscribe({
+      next: (data) => {console.log(data)},
+      complete: () => {console.log("usunięto przyjaciela")},
+      error: (err) => {console.log(err)}
+    })
+  }
+
+  addToFriend(senderID: number, invitationID: number): void{
+    const userID = decodeJWTToken().sub.low;
+    this.userProfileService.addFriend(senderID, userID).subscribe({
+      next: (data) => {console.log(data)},
+      complete: () => { console.log("dodano do znajomych"); this.deleteInvitation(invitationID); window.location.reload();},
+      error: (err) => { console.log(err)}
+    })
+  }
 }
