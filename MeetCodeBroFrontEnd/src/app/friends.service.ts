66b909aacc6017f8b483a0ad86a5938ebe338557
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-25 15:59:24 +0200

    creating a service to modify friends

---
+++ MeetCodeBroFrontEnd/src/app/friends.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, tap, map} from 'rxjs/operators';
+import { Invite } from './invitations';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendsService {
+
+  private friendsUrl = '/api/friends';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getFriendInvitation(userID: number): Observable<Invite[]>{
+    return this.http.get<Invite[]>(`${this.friendsUrl}/getInvitation/${userID}`, this.httpOptions )
+  }
+
+}
