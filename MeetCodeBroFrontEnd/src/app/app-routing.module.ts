57b94e92b6d9feba7e36d846130508ab89936004
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-26 16:21:41 +0200

    add user catalog

--- MeetCodeBroFrontEnd/src/app/app-routing.module.ts
+++ MeetCodeBroFrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserCatalogComponent } from './user-catalog/user-catalog.component';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent },
   {path: 'login', component: LoginComponent},
-  {path: 'protected', component: UserProfileComponent}
+  {path: 'protected', component: UserProfileComponent},
+  {path: 'catalog', component: UserCatalogComponent}
 ];
 
 @NgModule({
