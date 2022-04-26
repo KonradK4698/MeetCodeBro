4054944ac557d15e6ca25da59d4bbe5f7562ce07
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-04-26 16:48:03 +0200

    get user count

--- MeetCodeBroFrontEnd/src/app/user-catalog/user-catalog.component.ts
+++ MeetCodeBroFrontEnd/src/app/user-catalog/user-catalog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { CatalogService } from '../catalog.service';
+import { limitSkip } from '../catalog';
+import { User } from '../user';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-user-catalog',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCatalogComponent implements OnInit {
 
-  constructor() { }
+  private userNumber: Number = 0;
+
+  constructor(private catalogService: CatalogService) { }
 
   ngOnInit(): void {
+    this.usersCount();
   }
 
+  usersCount(): void{
+    this.catalogService.getUsersCount().subscribe({
+      next: (data) => {this.userNumber = data},
+      complete: () => {console.log("pobrano liczbę użytkowników " + this.userNumber)},
+      error: (err) => {console.log(err)}
+    })
+  }
 }
