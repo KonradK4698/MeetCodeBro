13495f6d7253e1b1bc969893ca11566bf41803c0
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-05-01 17:25:32 +0200

    add show user in routing

--- MeetCodeBroFrontEnd/src/app/app-routing.module.ts
+++ MeetCodeBroFrontEnd/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserCatalogComponent } from './user-catalog/user-catalog.component';
+import { ShowUserComponent } from './show-user/show-user.component';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent },
   {path: 'login', component: LoginComponent},
-  {path: 'protected', component: UserProfileComponent},
-  {path: 'catalog', component: UserCatalogComponent}
+  {path: 'account', component: UserProfileComponent},
+  {path: 'catalog', component: UserCatalogComponent},
+  {path: 'user/:id', component: ShowUserComponent}
 ];
 
 @NgModule({
