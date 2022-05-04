eee4caa5e8ab2f79ec2d1bcd4026c6af2aec9ffd
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-05-04 15:35:01 +0200

    create endpoint to get user known technologies

--- routes/technologies.js
+++ routes/technologies.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
 const driver = require('../db');
 
@@ -23,6 +24,27 @@ router.get('/', (req,res)=>{
 
     })
 
+})
+
+router.get('/:userID', (req,res)=>{
+
+    const session = driver.session(); 
+    let userTechnology = []; 
+    const userID = req.params.userID;
+    session.run(`MATCH (u:User)-[k:Know]->(t:Technologies) WHERE ID(u) = ${userID} RETURN t AS tech ORDER BY t.name ASC`).subscribe({
+        onNext: (data) => {
+            const technology = data.get('tech');
+
+            let getTechnology = {
+                ID:  technology.identity.low, 
+                Name: technology.properties.name
+            }
+
+            userTechnology.push(getTechnology);
+        },
+        onCompleted: () => {res.status(200).send(userTechnology); session.close()},
+        onError: (err) => {console.log(err)}
+    })
 })
 
 
